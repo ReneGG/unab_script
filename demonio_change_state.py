b4461d3d136862a46e9f4895989a9e2bd58cfda3
ReneGG <renegalarcegodoy@gmail.com>
2025-07-15 09:28:52 -0400

    actualiza wuery cambia estado

--- demonio_change_state.py
+++ demonio_change_state.py
@@ -11,14 +11,17 @@ from datetime import date, datetime, time, timedelta
 
 #datos fecha para crear
 date_format = "%Y-%m-%d %H:%M:%S.0"
-now = datetime.today()
+now = datetime.today().date()
 #fin datos fecha para crear
 conexion_sgr = psycopg2.connect(host=config.host,database=config.database_sgr,user=config.user_sgr,password=config.password_sgr)
 base_schedule = conexion_sgr.cursor()
 
-#dejo todos los registros anteriors a la fecha actual como Finalizada
-records_update = conexion_sgr.cursor()
-records_update.execute("UPDATE schedule_schedule SET state = 'Finalizada' WHERE reservation_fin < '2025-07-14'")
+#Inicia reservas
+records_update_init = conexion_sgr.cursor()
+records_update_init.execute("UPDATE schedule_schedule SET state = 'Iniciada' WHERE reservation_init = now and state = 'Aceptada'")
+#Finaliza reservas
+records_update_final = conexion_sgr.cursor()
+records_update_final.execute("UPDATE schedule_schedule SET state = 'Finalizada' WHERE reservation_fin < now")
 conexion_sgr.commit()   
 conexion_sgr.close()
 
