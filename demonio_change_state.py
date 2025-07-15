246a857e78a37a98de001d1e872d309808a1d8f1
ReneGG <renegalarcegodoy@gmail.com>
2025-07-15 09:39:19 -0400

    actualiza wuery cambia estado corrige error variable

--- demonio_change_state.py
+++ demonio_change_state.py
@@ -12,16 +12,17 @@ from datetime import date, datetime, time, timedelta
 #datos fecha para crear
 date_format = "%Y-%m-%d %H:%M:%S.0"
 now = datetime.today().date()
+now_str = now.strftime('%Y-%m-%d')
 #fin datos fecha para crear
 conexion_sgr = psycopg2.connect(host=config.host,database=config.database_sgr,user=config.user_sgr,password=config.password_sgr)
 base_schedule = conexion_sgr.cursor()
 
 #Inicia reservas
 records_update_init = conexion_sgr.cursor()
-records_update_init.execute("UPDATE schedule_schedule SET state = 'Iniciada' WHERE reservation_init = now and state = 'Aceptada'")
+records_update_init.execute("UPDATE schedule_schedule SET state = 'Iniciada' WHERE reservation_init = %s and state = 'Aceptada'",(now_str,))
 #Finaliza reservas
 records_update_final = conexion_sgr.cursor()
-records_update_final.execute("UPDATE schedule_schedule SET state = 'Finalizada' WHERE reservation_fin < now")
+records_update_final.execute("UPDATE schedule_schedule SET state = 'Finalizada' WHERE reservation_fin < %s",(now_str,))
 conexion_sgr.commit()   
 conexion_sgr.close()
 
